2b25dbff3fa5eab6bad25aa3628909a89e5c18d5
zueva.elena <zueva.elena@zozo.com>
2018-10-05 14:09:07 +0900

    set view and viewmodel initial state; add view model tests base

--- app/src/main/java/dev/sample/authentication/ContentViewModel.kt
+++ app/src/main/java/dev/sample/authentication/ContentViewModel.kt
@@ -1,13 +1,11 @@
 package dev.sample.authentication
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dev.sample.authentication.model.User
 
-class ContentViewModel: ViewModel() {
-    val userData = MutableLiveData<User>()
-
-    fun test() {
-        userData.observeForever {  }
-    }
-}
+/**
+ * Holds data-handling business logic of content screen.
+ */
+class ContentViewModel: ViewModel()
--- app/src/test/java/dev/sample/authentication/ContentViewModelTest.kt
+++ app/src/test/java/dev/sample/authentication/ContentViewModelTest.kt
@@ -9,9 +9,33 @@ import org.junit.Assert.*
  */
 class ContentViewModelTest {
 
+    @Test
+    fun whenNotLoggedIn_getSignInIntent_success() {
+        // TODO implement the test
+    }
 
     @Test
-    fun username_whenNotLoggedIn_equalDefaultValue() {
+    fun whenNotLoggedIn_doLogout_error() {
+        // TODO implement the test
+    }
+
+    @Test
+    fun whenDoLogInAndGetError_informAbout() {
+        // TODO implement the test
+    }
+
+    @Test
+    fun whenLoggedIn_getSignInIntent_success() {
+        // TODO implement the test
+    }
+
+    @Test
+    fun whenLoggInStateChanged_userDataRetrieved() {
+        // TODO implement the test
+    }
+
+    @Test
+    fun whenDoLogOutAndGetError_informAbout() {
         // TODO implement the test
     }
 }
