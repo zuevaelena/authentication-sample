23494abca1f3398527c192c2632978455188246d
zueva.elena <zueva.elena@zozo.com>
2018-10-11 12:56:10 +0900

    implement user class, containing logged in state logic

--- app/src/main/java/dev/sample/authentication/model/User.kt
+++ app/src/main/java/dev/sample/authentication/model/User.kt
@@ -1,5 +1,15 @@
 package dev.sample.authentication.model
 
-data class User(
-        val name: String
-        , var isLoggedIn: Boolean = false)
+import android.net.Uri
+
+data class User(val id: String, val name: String, var photoUrl: Uri) {
+    companion object {
+        private const val ANONYMOUS_USER_ID = "______no-user______"
+        private const val ANONYMOUS_USER_NAME = "Anonymous"
+
+        fun anonymousUserId() = ANONYMOUS_USER_ID
+        fun anonymousUserName() = ANONYMOUS_USER_NAME
+    }
+
+    fun isLoggedIn(): Boolean = id != ANONYMOUS_USER_ID
+}
