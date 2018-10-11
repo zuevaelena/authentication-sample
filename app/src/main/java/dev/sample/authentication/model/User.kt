3b95851eea73029109897deb0df2cfb34a1036c8
zueva.elena <zueva.elena@zozo.com>
2018-10-11 15:30:19 +0900

    use glide to display user photo

--- app/src/main/java/dev/sample/authentication/model/User.kt
+++ app/src/main/java/dev/sample/authentication/model/User.kt
@@ -1,11 +1,13 @@
 package dev.sample.authentication.model
 
-data class User(val id: String, val name: String, var photoUrl: String) {
+import android.net.Uri
+
+data class User(val id: String, val name: String, var photoUrl: Uri) {
     companion object {
         private const val ANONYMOUS_USER_ID = "______no-user______"
         private const val ANONYMOUS_USER_NAME = "Anonymous"
 
-        fun getAnonymous() : User = User(ANONYMOUS_USER_ID, ANONYMOUS_USER_NAME, "")
+        fun getAnonymous() : User = User(ANONYMOUS_USER_ID, ANONYMOUS_USER_NAME, Uri.EMPTY)
     }
 
     fun isLoggedIn(): Boolean = id != ANONYMOUS_USER_ID
--- app/src/main/java/dev/sample/authentication/ui/content/ContentFragment.kt
+++ app/src/main/java/dev/sample/authentication/ui/content/ContentFragment.kt
@@ -9,6 +9,8 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import dev.sample.authentication.R
 import dev.sample.authentication.databinding.FragmentContentBinding
 import dev.sample.authentication.di.DaggerContentFragmentComponent
@@ -43,6 +45,14 @@ class ContentFragment : Fragment() {
         val binding: FragmentContentBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_content, container, false)
         binding.apply {
             user = viewModel.user
+
+            Glide.with(this@ContentFragment)
+                    .setDefaultRequestOptions(RequestOptions().apply{
+                        placeholder(R.drawable.ic_account_circle)
+                        circleCrop()
+                    })
+                    .load(viewModel.user.photoUrl)
+                    .into(userPhoto)
         }
 
         return binding.root
