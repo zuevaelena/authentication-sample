bd0189621ea6a5649fa35d5f4e39d8da26e2a8cd
zueva.elena <zueva.elena@zozo.com>
2018-10-17 18:16:33 +0900

    change instantiation of Intent object for ContentViewModel.getSignInIntent() method test

--- app/src/test/java/dev/sample/authentication/fakeobjects/FakeSignInHandler.kt
+++ app/src/test/java/dev/sample/authentication/fakeobjects/FakeSignInHandler.kt
@@ -2,9 +2,8 @@ package dev.sample.authentication.fakeobjects
 
 import android.content.Intent
 import dev.sample.authentication.SignInHandler
+import org.mockito.Mockito.mock
 
 class FakeSignInHandler : SignInHandler {
-    override fun makeSignInIntent(): Intent {
-        return Intent()
-    }
+    override fun makeSignInIntent(): Intent = mock(Intent::class.java)
 }
