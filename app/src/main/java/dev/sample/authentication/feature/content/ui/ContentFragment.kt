b2151a7142c7421992da72bbf1d91976786373d4
zueva.elena <zueva.elena@zozo.com>
2018-10-22 19:03:37 +0900

    separate functionality by features - step5, cleaning up - put data binding back to content fragment

--- app/src/main/java/dev/sample/authentication/feature/content/ui/ContentFragment.kt
+++ app/src/main/java/dev/sample/authentication/feature/content/ui/ContentFragment.kt
@@ -1,10 +1,10 @@
 package dev.sample.authentication.feature.content.ui
 
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
@@ -12,9 +12,8 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import dagger.android.support.DaggerFragment
 import dev.sample.authentication.R
+import dev.sample.authentication.databinding.FragmentContentBinding
 import dev.sample.authentication.entity.User
-import kotlinx.android.synthetic.main.fragment_content.user_name
-import kotlinx.android.synthetic.main.fragment_content.user_photo
 import javax.inject.Inject
 
 
@@ -31,6 +30,7 @@ class ContentFragment : DaggerFragment() {
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
     private lateinit var viewModel: ContentViewModel
+    private lateinit var binding: FragmentContentBinding
 
     private var userObserver: Observer<User> = Observer { _ -> updateUi() }
 
@@ -39,21 +39,16 @@ class ContentFragment : DaggerFragment() {
 
         viewModel = ViewModelProviders.of(this, viewModelFactory)[ContentViewModel::class.java]
 
-        // TODO put back data binding
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_content, container, false)
+        binding.setLifecycleOwner(this@ContentFragment)
 
-        val contentView: View = inflater.inflate(R.layout.fragment_content, container, false)
+        updateUi()
 
-        // TODO put it back
-        // updateUi()
-
-        return contentView
+        return binding.root
     }
 
     override fun onStart() {
         super.onStart()
-
-        // TODO delete it
-        updateUi()
 
         viewModel.userData.observe(this, userObserver)
     }
@@ -65,19 +60,19 @@ class ContentFragment : DaggerFragment() {
     }
 
     private fun updateUi() {
-        if (user_name == null) return
+        binding.apply {
+            user = viewModel.userData.value
 
-        user_name.text = viewModel.userData.value?.name ?: ""
-
-        // TODO do it properly
-        Glide.with(this@ContentFragment)
-                .setDefaultRequestOptions(RequestOptions().apply {
-                    //placeholder(R.drawable.ic_account_circle) // TODO show something while loading
-                    error(R.drawable.ic_account_circle)
-                    circleCrop()
-                })
-                .load(viewModel.userData.value?.photoUrl ?: Uri.EMPTY)
-                .into(user_photo)
+            // TODO do it properly
+            Glide.with(this@ContentFragment)
+                    .setDefaultRequestOptions(RequestOptions().apply {
+                        //placeholder(R.drawable.ic_account_circle) // TODO show something while loading
+                        error(R.drawable.ic_account_circle)
+                        circleCrop()
+                    })
+                    .load(viewModel.userData.value?.photoUrl)
+                    .into(userPhoto)
+        }
     }
 
 }
