d3d32cfecaba3392f8b73c44c1837971dea899cb
zueva.elena <zueva.elena@zozo.com>
2018-10-23 12:05:20 +0900

    separate functionality by features - step5, cleaning up - use binding adapder for content screen

--- app/src/main/java/dev/sample/authentication/features/content/ui/ContentFragment.kt
+++ app/src/main/java/dev/sample/authentication/features/content/ui/ContentFragment.kt
@@ -8,8 +8,6 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import dagger.android.support.DaggerFragment
 import dev.sample.authentication.R
 import dev.sample.authentication.databinding.FragmentContentBinding
@@ -37,7 +35,7 @@ class ContentFragment : DaggerFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        viewModel = ViewModelProviders.of(this, viewModelFactory)[ContentViewModel::class.java]
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(ContentViewModel::class.java)
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_content, container, false)
         binding.setLifecycleOwner(this@ContentFragment)
@@ -57,16 +55,6 @@ class ContentFragment : DaggerFragment() {
     private fun updateUi() {
         binding.apply {
             user = viewModel.userData.value
-
-            // TODO do it properly
-            Glide.with(this@ContentFragment)
-                    .setDefaultRequestOptions(RequestOptions().apply {
-                        placeholder(R.drawable.ic_account_circle)
-                        error(R.drawable.ic_account_circle)
-                        circleCrop()
-                    })
-                    .load(viewModel.userData.value?.photoUrl)
-                    .into(userPhoto)
         }
     }
 
