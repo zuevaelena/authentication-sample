945e81f618afff607f8a2d3c6115765042dabcdf
zueva.elena <zueva.elena@zozo.com>
2018-10-29 17:30:25 +0900

    implement sign in result check tests; clean up content viewmodel tests

--- app/src/test/java/dev/sample/authentication/fakeobjects/FakeMakeSignInIntent.kt
+++
@@ -1,9 +0,0 @@
-package dev.sample.authentication.fakeobjects
-
-import android.content.Intent
-import dev.sample.authentication.features.bottommenu.usecase.MakeSignInIntent
-import org.mockito.Mockito.mock
-
-class FakeMakeSignInIntent : MakeSignInIntent {
-    override fun execute(): Intent = mock(Intent::class.java)
-}
