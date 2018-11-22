c581f97c5511f24487758cd2b3d5db5338f1d532
zueva.elena <zueva.elena@zozo.com>
2018-11-22 15:11:16 +0900

    implement user data mapper; add one more todo

--- app/src/main/java/dev/sample/authentication/data/remote/NewsPageResponse.kt
+++
@@ -1,8 +0,0 @@
-package dev.sample.authentication.data.remote
-
-import dev.sample.authentication.domain.model.News
-
-data class NewsPageResponse(
-        val status: String
-        , val totalResults: Int
-        , val articles: List<News>)
